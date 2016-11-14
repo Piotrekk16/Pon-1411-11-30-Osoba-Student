1fb0491d66337f09f984cd50ea19cfd7ac1043a5
Piotrekk85 <piotr.jastr@gmail.com>
2016-11-14 15:44:31 +0100

    Add files via upload

---
+++ ConsoleApplication13/ConsoleApplication13/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Osoba osoba1 = new Osoba("Jan", "Kowalski");
+            //Student student1 = new Student("Anna","Nowak",3,2,22345);
+            //osoba1.WypiszInfo();
+            //student1.WypiszInfo();
+
+            Osoba student2 = new Student("Tomasz","Nowak",3,2,11234);
+            student2.WypiszInfo();
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ ConsoleApplication13/ConsoleApplication13/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Student:Osoba
+    {
+        private int rokStudiow;
+        private int numerGrupy;
+        private int numerAlbumu;
+
+        public Student() { }
+
+        public Student(string imie, string nazwisko, int rokStudiow, int numerGrupy, int numerAlbumu)
+            :base(imie,nazwisko)
+        {
+            this.rokStudiow = rokStudiow;
+            this.numerGrupy = numerGrupy;
+            this.numerAlbumu = numerAlbumu;
+
+        }
+
+        public override void WypiszInfo()
+        {
+            base.WypiszInfo();
+            Console.WriteLine("Rok studiów: {0}", rokStudiow);
+            Console.WriteLine("Numer grupy: {0}", numerGrupy);
+            Console.WriteLine("Numer albumu: {0}", numerAlbumu);
+        }
+    }
+}
